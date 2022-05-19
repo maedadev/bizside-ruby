e7aeed712f2a348ac4ab1cb5dc02c6c042e88735
Takuya Noguchi <takuya@netlab.jp>
2022-05-19 13:53:40 +0900

    StringUtils.create_random_alpha_string の生成する文字の上限を廃止
    * 文字集合から1文字ずつしか使用出来ない、という挙動となっていたため修正前は以下のような制約があった
      * 英小文字の場合: 上限26文字
      * 英大・小文字の場合: 上限52文字

--- bizside_test_app/test/lib/bizside/string_utils_test.rb
+++ bizside_test_app/test/lib/bizside/string_utils_test.rb
@@ -7,4 +7,21 @@ class Bizside::StringUtilsTest < ActiveSupport::TestCase
     assert s =~ /^(?=.*?[a-z])(?=.*?\d).*+$/iu
   end
 
+  def test_create_random_alpha_string_case_sensitive_true
+    s = StringUtils.create_random_alpha_string(10, true)
+    assert s =~ /\A[A-Za-z]{10}\z/
+  end
+
+  def test_create_random_alpha_string_case_sensitive_false
+    s = StringUtils.create_random_alpha_string(10, false)
+    assert s =~ /\A[a-z]{10}\z/
+  end
+
+  def test_create_random_alpha_string_more_than_26_chars
+    s = StringUtils.create_random_alpha_string(53, true)
+    assert s =~ /\A[A-Za-z]{53}\z/
+
+    s = StringUtils.create_random_alpha_string(27, false)
+    assert s =~ /\A[a-z]{27}\z/
+  end
 end
