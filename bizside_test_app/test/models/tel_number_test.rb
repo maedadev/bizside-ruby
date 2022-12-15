df45f2937959eebd0db4691c67a797ed196dd8b8
m-shimojo <m.shimojyo@bizside.biz>
2022-12-15 13:00:22 +0900

    Add tests for TelValidator and ZipValidator

--- bizside_test_app/test/models/tel_number_test.rb
+++ bizside_test_app/test/models/tel_number_test.rb
@@ -2,6 +2,19 @@ require 'test_helper'
 
 class TelNumberTest < ActiveSupport::TestCase
 
+  def test_正当な電話番号
+    assert TelNumber.new(tel: '0120-1234-5678').valid?
+    assert TelNumber.new(tel: '012012345678').valid?
+    assert TelNumber.new(tel: '０１２０－１２３４－５６７８').valid?
+    assert TelNumber.new(tel: '０１２０１２３４５６７８').valid?
+    assert TelNumber.new(tel: nil).valid?
+  end
+
+  def test_不正な郵便番号
+    assert TelNumber.new(tel: '(0120) 1234-5678').invalid?
+    assert TelNumber.new(tel: '〇一二〇-一二三四-五六七八').invalid?
+  end
+
   def test_エラーメッセージ
     tel_number = TelNumber.new(tel: 'xxx')
     assert tel_number.invalid?
