55a4fe10fbfdcb45d99443916ae2e807a0494498
ta-ando <t.ando@bizside.biz>
2022-12-19 15:24:40 +0900

    refs #53 警告が出ないよう定数を削除してから上書きする

--- lib/bizside/user_agent/action_view/use_variant.rb
+++ lib/bizside/user_agent/action_view/use_variant.rb
@@ -1,4 +1,6 @@
 require 'action_view/template/resolver'
 
+ActionView::PathResolver.instance_eval { remove_const :EXTENSIONS }
+ActionView::PathResolver.instance_eval { remove_const :DEFAULT_PATTERN }
 ActionView::PathResolver::EXTENSIONS = { locale: ".", variants: ".", formats: ".",  handlers: "." }
 ActionView::PathResolver::DEFAULT_PATTERN = ":prefix/:action{.:locale,}{.:variants,}{.:formats,}{.:handlers,}"
