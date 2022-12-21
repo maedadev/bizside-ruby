4f1663e9871228d1ce29a1eabfb80e6db28d5f41
ta-ando <t.ando@bizside.biz>
2022-12-21 15:26:15 +0900

    refs #54 テスト追加

--- bizside_test_app/test/controllers/top_controller_test.rb
+++ bizside_test_app/test/controllers/top_controller_test.rb
@@ -5,17 +5,27 @@ class TopControllerTest < ActionDispatch::IntegrationTest
     get '/'
     assert_response :success
     assert_template :"show"
+    assert response.body.include?('show.html.erb')
   end
 
   def test_トップ_pc
     get '/', params: {ua: :pc}
     assert_response :success
     assert_template :"show"
+    assert response.body.include?('show.html.erb'), 'show.pc.html.erb は無いので show.html.erb を表示する'
   end
 
   def test_トップ_iphone
     get '/', params: {ua: :iphone}
     assert_response :success
     assert_template :"show.iphone"
+    assert response.body.include?('show.iphone.html.erb')
+  end
+
+  def test_トップ_android
+    get '/', params: {ua: :android}
+    assert_response :success
+    assert_template :"show.sp"
+    assert response.body.include?('show.sp.html.erb'), 'show.android.html.erb は無いので show.sp.html.erb を表示する'
   end
 end
